f3b84590adb0ce57ca906861bea798ab778a2850
App Academy Student <student@appacademy.io>
2013-11-26 17:09:31 -0800

    Stepping and Sliding pieces working with board display

--- board.rb
+++ board.rb
@@ -10,6 +10,21 @@ class Board
     @board = Array.new(8) { Array.new(8, nil) }
   end
 
+  def to_s
+    output = ""
+    @board.each_index do |row_index|
+      @board.each_index do |col_index|
+        if @board[col_index][row_index] == nil
+          output += "_"
+        else
+          output += @board[col_index][row_index].to_s
+        end
+      end
+      output += "\n"
+    end
+    output
+  end
+
   def [](x,y)
     @board[x][y]
   end
@@ -24,23 +39,28 @@ class Board
   end
 
   def capture(x, y)
-    @board[end_pos[0], end_pos[1]] = nil
+    @board[x][y] = nil
   end
 
   def move_piece(start_pos, end_pos)
-    current_piece = @board[start_pos[0], start_pos[1]]
-    if current_piece.move.include?([end_pos[0], end_pos[1]])
-      if has_piece?([end_pos[0], end_pos[1]]) && enemy?
+    current_piece = @board[start_pos[0]][start_pos[1]]
+    p end_pos
+    if current_piece.moves.include?(end_pos)
+      if has_piece?(end_pos)
         capture(end_pos[0], end_pos[1])
       end
+      @board[end_pos[0]][end_pos[1]] = current_piece
+      current_piece.pos = end_pos
+      @board[start_pos[0]][start_pos[1]] = nil
     end
-
   end
-
 end
 
 b = Board.new
-rook = Rook.new([0,0], :black, b)
-b[0, 0] = rook
-b[0, 5] = Rook.new([0,5], :black, b)
-p rook.moves
+q = King.new([2,3], :black, b)
+p q.moves
+b[2, 3] = q
+b[3, 4] = Knight.new([3,4], :white, b)
+p b
+b.move_piece([2, 3], [3, 4])
+p b
--- piece.rb
+++ piece.rb
@@ -1,7 +1,8 @@
 require 'debugger'
 
 class Piece
-  attr_reader :moves, :pos, :color
+  attr_accessor :pos
+  attr_reader :moves, :color
 
   def initialize(pos, color, board)
     @pos = pos
@@ -20,7 +21,6 @@ end
 
 
 class SlidingPiece < Piece
-
   def moves
     all_moves = []
 
@@ -42,18 +42,87 @@ class SlidingPiece < Piece
         new_move = [ pos[0] + (dir[0] * step), pos[1] + (dir[1] * step) ]
       end
     end
+    all_moves
+  end
+end
+
+#knights, king
+class SteppingPiece < Piece
+  def moves
+    all_moves = []
+
+    @move_dirs.each do |dir|
+      new_move = [pos[0] + dir[0], pos[1] + dir[1]]
+      next unless in_bounds?(new_move)
+      unless @board.has_piece?(new_move) &&
+             @color == @board[new_move[0], new_move[1]].color
+        all_moves << new_move
+      end
+    end
 
     all_moves
   end
-
 end
 
 class Rook < SlidingPiece
-  attr_reader :move_dirs
 
   def initialize(pos, color, board)
-    super(pos, color, board)
+    super
     @move_dirs = [ [1, 0] , [0, 1], [-1, 0], [0, -1] ]
   end
 
+  def to_s
+    "R"
+  end
+end
+
+class Bishop < SlidingPiece
+
+  def initialize(pos, color, board)
+    super
+    @move_dirs = [ [1, 1] , [-1, 1], [-1, -1], [1, -1] ]
+  end
+
+  def to_s
+    "B"
+  end
+end
+
+class Queen < SlidingPiece
+
+  def initialize(pos, color, board)
+    super
+    @move_dirs = [ [1, 1] , [-1, 1], [-1, -1], [1, -1],
+                   [1, 0] , [0, 1], [-1, 0], [0, -1] ]
+  end
+
+  def to_s
+    "Q"
+  end
+end
+
+class King < SteppingPiece
+
+  def initialize(pos, color, board)
+    super
+    @move_dirs = [ [1, 1] , [-1, 1], [-1, -1], [1, -1],
+                   [1, 0] , [0, 1], [-1, 0], [0, -1] ]
+  end
+
+  def to_s
+    "M"
+  end
+end
+
+class Knight < SteppingPiece
+
+  def initialize(pos, color, board)
+    super
+    @move_dirs = [ [2, 1], [1, 2], [-1, 2], [-2, 1],
+                   [-2, -1], [-1, -2], [1, -2], [2, -1]]
+  end
+
+  def to_s
+    "K"
+  end
 end
